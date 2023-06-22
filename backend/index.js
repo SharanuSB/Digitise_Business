15aa7624fbfbae1a04f307402eb10a35adaacb77
Sharanu <sharanusb12345@gmail.com>
2023-06-22 17:40:55 +0530

    Modified routes and Index file

--- backend/index.js
+++ backend/index.js
@@ -1,11 +1,13 @@
 const express  = require("express")
 const cors = require("cors")
 const configureDB = require("./config/database")
+const router = require("./config/routes")
 const PORT = 3333
 
 const app = express()
 
 app.use(cors())
+app.use(router)
 
 app.listen(PORT, (req, res)=>{
     console.log("Server is running on port", PORT)
