82794ab636a67500dd2791298cbdabfdba863187
Sharanu <sharanusb12345@gmail.com>
2023-06-23 18:22:46 +0530

    userController

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,11 @@
 const express = require("express")
+const usersController = require("../app/controllers/usersController")
 
 const router = express.Router()
+
+/// Api's For User Model -------
+router.post("/api/users/register", usersController.register)
+router.post("/api/users/login", usersController.login)
 
 
 module.exports = router
