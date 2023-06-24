b85f01ccab399f4db9c2f4517d9ba5901fe36b28
Nikhil <patilnd882000@gmail.com>
2023-06-24 14:53:21 +0530

    created category controller and route setup

--- backend/config/routes.js
+++ backend/config/routes.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const usersController = require("../app/controllers/usersController")
 const userAuthentication = require("../app/middlewares/userAuthentication")
+const categoryController = require("../app/controllers/categoryController")
 
 const router = express.Router()
 
@@ -9,5 +10,10 @@ router.post("/api/users/register", usersController.register)
 router.post("/api/users/login", usersController.login)
 router.put("/api/users/update", userAuthentication, usersController.update)
 
+/// Api's For Category Model ------
+router.post("/api/categories", userAuthentication, (req, res, next) => {
+    req.permittedRoles = ['shopOwner']
+    next()
+}, categoryController.create)
 
 module.exports = router
